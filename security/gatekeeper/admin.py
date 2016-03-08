fa61f5727670e72b73992b9af94d962357f1ca35
Yogesh <yogesh.chaurse@weboniselab.com>
2016-03-09 00:39:53 +0530

    Completed User and Adress module

--- security/gatekeeper/admin.py
+++ security/gatekeeper/admin.py
@@ -1,11 +1,49 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Address
 from django.contrib.admin import AdminSite
 from django.utils.translation import ugettext_lazy
 # Register your models here.
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = (
+        'first_name',
+        'last_name',
+        'email',
+    )
+    list_display = [
+        'first_name',
+        'last_name',
+        'email',
+        'contact_number',
+    ]
+
+    search_fields = (
+        'email', 'first_name',
+    )
 
 
+class AddressAdmin(admin.ModelAdmin):
 
+    fields = (
+        'user',
+        'address_line_1',
+        'address_line_2',
+        'building_no',
+        'floor_no',
+        'house_no',
+        'city',
+        'state',
+        'pin_code')
 
+    list_display = [
+        'user',
+        'city',
+        'state'
+    ]
+    search_fields = (
+        'user', 'floor_no', 'house_no'
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Address, AddressAdmin)
--- security/gatekeeper/models.py
+++ security/gatekeeper/models.py
@@ -4,14 +4,88 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(models.Model):
-    email = models.EmailField()
-    first_name = models.CharField(max_length=8)
-    last_name = models.CharField(max_length=8)
+    email = models.EmailField(
+        blank=False,
+        null=False,
+        unique=True)
+
+    first_name = models.CharField(
+        max_length=8,
+        blank=False,
+        null=False)
+
+    last_name = models.CharField(
+        max_length=8,
+        null=False,
+        blank=False)
+
     contact_number = models.IntegerField(default=0)
     is_active = models.BooleanField
     dob = models.DateField
 
-
-    def __str__ (self):
+    def __str__(self):
         return self.first_name
+
+
+class Address(models.Model):
+
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name='addresses')
+
+    address_line_2 = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
+    address_line_1 = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False
+    )
+
+    building_no = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+
+    floor_no = models.IntegerField(
+        blank=True,
+        null=True
+    )
+
+    house_no = models.CharField(
+        max_length=25,
+        blank=False,
+        null=False
+    )
+
+    city = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    state = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    pin_code = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+
+
+
+
